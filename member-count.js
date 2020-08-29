c7607ca282ce85e43463eef0b41db3198fc168fd
blackcoralz <vincentadnto@gmail.com>
2020-08-29 20:42:48 +0700

    Some Update

--- member-count.js
+++ member-count.js
@@ -1,9 +1,17 @@
 module.exports = (bot) => {
     const channelId = '749148224908165130'
+    const channelId2 = '749256267452776488'
+    const channelId3 = '749258366563647498'
 
     const updateMembers = (guild) => {
         const channel = guild.channels.cache.get(channelId)
+        const channel2 = guild.channels.cache.get(channelId2)
+        const channel3 = guild.channels.cache.get(channelId3)
+        const booster = guild.members.cache.filter(m => m.roles.cache.has('735856052251263069')).size;
+        const botuser = guild.members.cache.filter(m => m.user.bot).size;
         channel.setName(`📊︱Member : ${guild.memberCount.toLocaleString()}`)
+        channel2.setName(`📊︱Booster : ${booster}`)
+        channel3.setName(`📊︱Bot : ${botuser}`)
     }
     
     bot.on('guildMemberAdd', (member) => updateMembers(member.guild))
